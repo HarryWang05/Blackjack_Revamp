28a589cc2a09390f80576513f555f84478a2af56
Harry Wang <yosharry1234@gmail.com>
2024-01-14 02:32:33 -0500

    Complete round functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,82 +1,151 @@
-import logo from './logo.svg';
-import './App.css';
-import Hand from './components/Hand';
-import Message from './components/Message';
-import {Deck, drawCard, createDecks, calcHand} from './Functions';
-import {useState} from 'react';
+import logo from "./logo.svg";
+import "./App.css";
+import Hand from "./components/Hand";
+import Message from "./components/Message";
+import { Deck, drawCard, createDecks, calcHand, isBust } from "./Functions";
+import { useState } from "react";
 
 let curDeck: Deck;
+let hiddenCard: number[]; // Dealer's first card
 
 function App() {
-  const [dealerHand, setDealerHand] = useState([[13,0]]);
-  const [playerHand, setPlayerHand] = useState([[13,0]]);
-  const [dealerEvent, setDealerEvent] = useState('');
-  const [playerEvent, setPlayerEvent] = useState('');
+	/*const [state, setState] = useState({
+		dealerHand: [[13,0]],
+		playerHand: [[13,0]],
+		dealerEvent: "",
+		playerEvent: "",
+		started: false,
+		playerOver: false
+	});*/
+  const [dealerHand, setDealerHand] = useState([[13, 0]]);
+  const [playerHand, setPlayerHand] = useState([[13, 0]]);
+  const [dealerEvent, setDealerEvent] = useState("");
+  const [playerEvent, setPlayerEvent] = useState("");
   const [started, setStarted] = useState(false);
-  const [playerOver, setPlayerOver] = useState(false);
-
-  let hiddenCard: number[]; // Dealer's first card
+  const [roundOver, setRoundOver] = useState(false);
   let cutoff = 0.5; // Decimal fraction of when to shuffle deck
 
   const startRound = () => {
     curDeck = createDecks(1);
     firstDeal();
     setStarted(true);
-  }
+  };
 
   const restartRound = () => {
-    if(curDeck.numCards <= 52*cutoff) {
+    if (curDeck.numCards <= 52 * cutoff) {
       curDeck = createDecks(1);
     }
     firstDeal();
-    setPlayerOver(false);
-  }
+    setRoundOver(false);
+		setDealerEvent('');
+		setPlayerEvent('');
+  };
 
   const firstDeal = () => {
-    let deal = [drawCard(curDeck), drawCard(curDeck), drawCard(curDeck), drawCard(curDeck)];
-    setPlayerHand([deal[0]]);
-    hiddenCard = deal[1]; // Store hidden card in variable
+    let deal = [
+      drawCard(curDeck),
+      drawCard(curDeck),
+      drawCard(curDeck),
+      drawCard(curDeck),
+    ];
+    hiddenCard = deal[1].slice(); // Store hidden card in variable
+		alert(JSON.stringify(hiddenCard) + "hiddenCardasdf");
     setPlayerHand([deal[0], deal[2]]);
-    setDealerHand([[13,0],deal[3]]); // [13,0] is 'X', the hidden card
-    setDealerEvent('playing');
-    // alert([...playerHand[0],...playerHand[1]]);
-    // alert([...dealerHand[0],...dealerHand[1]]);
-  }
+    setDealerHand([[13, 0], deal[3]]); // [13,0] is 'X', the hidden card
+    setDealerEvent("playing");
+  };
 
   const doHit = () => {
     alert(JSON.stringify(curDeck));
     let drawn = drawCard(curDeck);
-    if(calcHand([...playerHand, drawn]) > 21) {
-      setPlayerEvent('Bust!');
-      setPlayerOver(true);
+    if (isBust([...playerHand, drawn])) {
+      setPlayerEvent("Bust!");
+			startDealer(true); // Player bust true
     }
-    setPlayerHand([
-      ...playerHand,
-      drawn
-    ]);
-  }
-  
+    setPlayerHand([...playerHand, drawn]);
+  };
+
   const doStand = () => {
-    setPlayerOver(true);
-  }
+    setRoundOver(true);
+		startDealer(false); // Player bust false
+  };
+
+	const startDealer = (isPlayerBust: Boolean) => {
+		alert("reached startDealer");
+		let tempHand = JSON.parse(JSON.stringify(dealerHand)); // Temp hand to store dealer hand
+		tempHand[0] = hiddenCard; // Reveal hidden card
+		if(isPlayerBust) {
+			dealerWin();
+		} else {
+			while(calcHand(tempHand) < 17) {
+				tempHand.push(drawCard(curDeck));
+			}
+			if(isBust(tempHand)) {
+				setDealerEvent("bust!");
+				playerWin();
+			} else if(calcHand(tempHand) < calcHand(playerHand)) {
+				playerWin();
+			} else if(calcHand(tempHand) > calcHand(playerHand)) {
+				dealerWin();
+			} else {
+				tie();
+			}
+		}
+		setDealerHand(tempHand);
+	}
+
+	const dealerWin = () => {
+		alert("reached dealerWin");
+		setDealerEvent("wins!");
+		setRoundOver(true);
+	}
+
+	const playerWin = () => {
+		setPlayerEvent("wins!");
+		setRoundOver(true);
+	}
+
+	const tie = () => {
+		setDealerEvent("ties!");
+		setPlayerEvent("ties!");
+		setRoundOver(true);
+	}
 
   return (
     <div className="App">
-      <div className="title">
-        Blackjack
-      </div>
+      <div className="title">Blackjack</div>
       <div className="game-area">
-        <hr/>
-        <Hand name="Dealer's" cards={dealerHand}/>
-        <Message name="Dealer" event={dealerEvent}/>
-        <hr/>
-        <Hand name="Your" cards={playerHand}/>
-        <Message name="Player" event={playerEvent}/>
+        <hr />
+        <Hand name="Dealer's" cards={dealerHand} />
+        <Message name="Dealer" event={dealerEvent} />
+        <hr />
+        <Hand name="Your" cards={playerHand} />
+        <Message name="Player" event={playerEvent} />
         <div className="buttons">
-          <button className={`${(started || playerOver) ? "start invis" : "start"}`} onClick={startRound}>Start</button>
-          <button className={`${(started && !playerOver) ? "hit" : "hit invis"}`} onClick={doHit}>Hit</button>
-          <button className={`${(started && !playerOver) ? "stand" : "stand invis"}`} onClick={doStand}>Stand</button>
-          <button className={`${playerOver ? "restart" : "restart invis"}`} onClick={restartRound}>Next</button>
+          <button
+            className={`${started || roundOver ? "start invis" : "start"}`}
+            onClick={startRound}
+          >
+            Start
+          </button>
+          <button
+            className={`${started && !roundOver ? "hit" : "hit invis"}`}
+            onClick={doHit}
+          >
+            Hit
+          </button>
+          <button
+            className={`${started && !roundOver ? "stand" : "stand invis"}`}
+            onClick={doStand}
+          >
+            Stand
+          </button>
+          <button
+            className={`${roundOver ? "restart" : "restart invis"}`}
+            onClick={restartRound}
+          >
+            Next
+          </button>
         </div>
       </div>
     </div>
--- src/Functions.tsx
+++ src/Functions.tsx
@@ -1,68 +1,71 @@
 export interface Deck {
-    numCards: number;
-    totalCards: number[][];
+  numCards: number;
+  totalCards: number[][];
 }
 
 let baseCards: number[][] = [];
 
 let fourKinds: number[] = [];
 
-for(let j = 0; j < 4; j++) {
-    fourKinds.push(0);
+for (let j = 0; j < 4; j++) {
+  fourKinds.push(0);
 } // fourKinds = [0, 0, 0, 0], one index for each suit
 // [0] = Spades, [1] = Hearts, [2] = Diamonds, [3] = Clubs
 
-for(let i = 0; i < 13; i++) {
-    baseCards.push(fourKinds.slice());
+for (let i = 0; i < 13; i++) {
+  baseCards.push(fourKinds.slice());
 } // 13 indices, one for each card value, with [0] = Aces ... [12] = Kings
 
 const randomInt = (num: number) => {
-    return Math.ceil(Math.random()*num);
-}
+  return Math.ceil(Math.random() * num);
+};
 
 export const drawCard = (deck: Deck) => {
-    /*let newDeck = {
-        numCards: deck.numCards,
-        totalCards: deck.totalCards.slice()
-    };*/
-    if(deck.numCards <= 0) {
-        return [13,0]; // Returns 'X' if there are no more cards in the deck
+  if (deck.numCards <= 0) {
+    return [13, 0]; // Returns 'X' if there are no more cards in the deck
+  }
+  let pickedCard = randomInt(deck.numCards);
+  for (let i = 0; i < 13; i++) {
+    for (let j = 0; j < 4; j++) {
+      pickedCard -= deck.totalCards[i][j];
+      if (pickedCard <= 0) {
+        deck.numCards--;
+        deck.totalCards[i][j]--;
+        return [i, j]; // returns [value,suit] (properties of card drawn)
+      }
     }
-    let pickedCard = randomInt(deck.numCards);
-    for(let i = 0; i < 13; i++) {
-        for(let j = 0; j < 4; j++) {
-            pickedCard -= deck.totalCards[i][j];
-            if(pickedCard <= 0) {
-                deck.numCards--;
-                deck.totalCards[i][j]--;
-                return [i, j]; // returns [value,suit] (properties of card drawn)
-            }
-        }
-    }
-    return [13,0];
-}
+  }
+  return [13, 0];
+};
 
 export const createDecks = (num: number) => {
-    let deck: Deck = {
-        numCards: num*52,
-        totalCards: baseCards.slice()
-    };
-    for(let i = 0; i < 13; i++) {
-        for(let j = 0; j < 4; j++) {
-            deck.totalCards[i][j] = num;
-        }
+  let deck: Deck = {
+    numCards: num * 52,
+    totalCards: JSON.parse(JSON.stringify(baseCards)),
+  };
+  for (let i = 0; i < 13; i++) {
+    for (let j = 0; j < 4; j++) {
+      deck.totalCards[i][j] = num;
     }
-    return deck;
-}
+  }
+  return deck;
+};
 
 export const calcHand = (hand: number[][]) => {
-    let total = 0;
-    for(let i = 0; i < hand.length; i++) {
-        if(hand[i][0] > 9) {
-            total += 10; // when face card
-        } else {
-            total += hand[i][0]+1; // face value
-        }
+  let total = 0;
+  for (let i = 0; i < hand.length; i++) {
+    if (hand[i][0] > 9) {
+      total += 10; // when face card
+    } else {
+      total += hand[i][0] + 1; // face value
     }
-    return total;
-}
+  }
+  return total;
+};
+
+export const isBust = (hand: number[][]) => {
+  if (calcHand(hand) > 21) {
+    return true;
+  }
+  return false;
+};
